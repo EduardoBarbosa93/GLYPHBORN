e14c90a79fce115c853ec7a505e618f1a98dfd9e
EduardoBarbosa93 <eduardfernandes511@gmail.com>
2026-09-10 23:56:57 -0300

    Atualizações do projetooo

--- TesteJokenpo/Program.cs
+++ TesteJokenpo/Program.cs
@@ -1,107 +1,150 @@
 ﻿
-for (int intro = 0; intro < 1; intro++)
+
+
+internal class TesteJokenpo
 {
-    Console.WriteLine("Digite seu nome, guerreiro: ");
-    string nome = Console.ReadLine()!;
-
-    Console.WriteLine("Digite sua idade: ");
-    int idade = Convert.ToInt32(Console.ReadLine());
-
-    if (idade < 13)
+    private static void Main(string[] args)
     {
-        Console.WriteLine("Você é muito jovem para jogar, " + nome + ". Volte quando tiver mais idade.");
+        Console.WriteLine("Digite seu nome, guerreiro: ");       /* Toda a introdução do jogo é feita aqui, respeitando a idade mínima e pedido de nome do usuário */
+        string nome = Console.ReadLine()!;
+
+        Console.WriteLine("Digite sua idade: ");
+        int idade = Convert.ToInt32(Console.ReadLine());
+
+        if (idade < 15)
+        {
+
+            Console.WriteLine("Você é muito jovem para jogar, " + nome + ". Volte quando tiver mais idade.");
+        }
+        else
+        {
+            Console.WriteLine("Bem-vindo ao jogo, " + nome + "! Vamos começar a batalha!");
+            Console.WriteLine("Bem vindo ao jokenpo mágico!!");
+            Console.WriteLine("Em um reino distante, um grande torneio era realizado todos os anos para decidir quem seria o maior campeão.");
+            Console.WriteLine("Três heróis foram escolhidos pelo rei para controlar as poderosas criaturas mágicas do reino e colocá-las para lutar.");
+            Console.WriteLine("Agora, chegou a sua vez, " + nome + ". Escolha sua fera, entre na arena e prove que você é o maior domador dessas terras!");
+            Console.WriteLine("Aperte qualquer tecla para iniciar a batalha!");
+            Console.ReadKey();
+            Console.Clear();                                                               // Após uma introdução breve, o combate se iniciaria, e assim a tela se limpa.
+            Console.WriteLine("========== NOVO COMBATE ==========");
+
+            int hpjogador = 10;
+            int hpcomputador = 10;
+            string[] jogadas = { "Unicórnio", "Pegasus", "Grifo" };                         // Início do combate, onde as variáveis de vida do player e da máquina são dadas, e as jogadas são definidas
+
+            while (hpjogador > 0 && hpcomputador > 0)                                                // Estrutura básica de combate, onde o jogador e a máquina vão se enfrentando até que um deles tenha o hp == 0            {
+            {
+                Random AtqComp = new Random();
+                int ataqueComp = AtqComp.Next(0, jogadas.Length);
+                string jogadaComp = jogadas[ataqueComp];
+
+                Console.WriteLine("Escolha sua criatura e ataque mágico: ");
+                Console.WriteLine("Unicórnio | Pegasus | Grifo");
+                string ataque = Console.ReadLine()!;
+                Console.WriteLine("Você escolheu: " + ataque);
+                Console.WriteLine("A máquina escolheu: " + jogadaComp);
+
+                switch (ataque)
+                {
+                    case "Unicórnio":
+                        if (jogadaComp == "Pegasus")
+                        {
+                            Console.WriteLine("O Pegasus é atingido pela luz desferida pelo unicórnio durante o voo. Unicórnio(jogador) vence a rodada.");
+                            hpcomputador -= 3;
+                            break;
+                        }
+                        else if (jogadaComp == "Grifo")
+                        {
+                            Console.WriteLine("O Grifo repele o ataque com suas garras, refletindo-o na direção do Unicórnio. Grifo(Computador) vence a rodada.");
+                            hpjogador -= 3;
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Empate! Nenhum dano ocorre.");
+                            break;
+                        }
+
+                    case "Pegasus":
+
+                        if (jogadaComp == "Grifo")
+                        {
+                            Console.WriteLine("O Grifo é cortado por uma arrancada do Pegasus vindo dos céus. Pegasus(jogador) vence a rodada.");
+                            hpcomputador -= 3;
+                            break;
+                        }
+                        else if (jogadaComp == "Unicórnio")
+                        {
+                            Console.WriteLine("O Pegasus é atingido pela luz desferida pelo unicórnio durante o voo. Unicórnio(Computador) vence a rodada.");
+                            hpjogador -= 3;
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Empate! Nenhum dano ocorre.");
+                            break;
+                        }
+
+                    case "Grifo":
+
+                        if (jogadaComp == "Unicórnio")
+                        {
+                            Console.WriteLine("O Grifo repele o ataque com suas garras, refletindo-o na direção do Unicórnio. Grifo(jogador) vence a rodada.");
+                            hpcomputador -= 3;
+                            break;
+                        }
+                        else if (jogadaComp == "Pegasus")
+                        {
+                            Console.WriteLine("O Grifo é cortado por uma arrancada do Pegasus vindo dos céus. Pegasus(Computador) vence a rodada.");
+                            hpjogador -= 3;
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Empate! Nenhum dano ocorre.");
+                            break;
+                        }
+
+                    default:
+                        Console.WriteLine("Escolha inválida. Tente novamente.");
+                        break;
+                }
+
+
+                string[,] hp =
+
+                {
+
+                 {"HP do Jogador -> ", hpjogador.ToString() },                               // Aqui é feito uma tela que mostra o HP do jogador e do adversário, usando uma pequena matriz
+                 {"HP do Computador -> ", hpcomputador.ToString() },
+
+                  };
+
+                Console.WriteLine("Status da batalha: ");
+                for (int i = 0; i < hp.GetLength(0); i++)
+
+                {
+                    Console.WriteLine(hp[i, 0] + hp[i, 1]);
+                }
+
+                Console.WriteLine("==================================");
+
+                if (hpjogador <= 0)
+                {
+                    Console.WriteLine("O computador venceu a batalha! Mais sorte na próxima vez, " + nome + ".");
+                }
+                else if (hpcomputador <= 0)
+                {
+                    Console.WriteLine("Parabéns, " + nome + "! Você venceu a batalha e se tornou o maior domador do reino!");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Pressione qualquer tecla para continuar...");
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+
+        }
     }
-    else
-    {
-        Console.WriteLine("Bem-vindo ao jogo, " + nome + "! Vamos começar a batalha!");
-        Console.WriteLine("Bem vindo ao jokenpo mágico!!");
-
-        Console.WriteLine("Em um reino distante, um grande torneio era realizado todos os anos para decidir quem seria o maior campeão.");
-        Console.WriteLine("Três heróis foram escolhidos pelo rei para controlar as poderosas criaturas mágicas do reino e colocá-las para lutar.");
-        Console.WriteLine("Agora, chegou a sua vez, " + nome + ". Escolha sua fera, entre na arena e prove que você é o maior domador dessas terras!");
-
-        Console.WriteLine("Aperte qualquer tecla para iniciar a batalha!");
-        Console.ReadKey();
-        Console.Clear();
-        Console.WriteLine("========== NOVO COMBATE ==========");
-        int hpjog = 10;
-        int hpcomp = 10;
-        string[] jogadas = { "Unicónio", "Pegasus", "Grifo" };
-
-        Random AtqComp = new Random();
-        int ataqueComp = AtqComp.Next(0, jogadas.Length);
-        string jogadaComp = jogadas[ataqueComp];
-
-        while (hpjog > 0 && hpcomp > 0)
-        {
-           
-            Console.WriteLine("Escolha sua criatura e ataque mágico: ");
-            Console.WriteLine("Unicórnio | Pegasus | Grifo");
-            string ataque = Console.ReadLine()!;
-
-            if (ataque == "Unicórnio" && jogadaComp == "Pegasus")
-            {
-                Console.WriteLine("O Pegasus é atingido pela luz desferida pelo unicórnio durante o voo. Unicórnio vence a rodada.");
-                hpcomp = hpcomp - 3;
-            }
-            else if (ataque == "Unicórnio" && jogadaComp == "Grifo")
-            {
-                Console.WriteLine("O Grifo repele o ataque com suas garras, refletindo-o na direção do Unicórnio. Grifo vence a rodada.");
-                hpjog = hpjog - 4;
-            }
-            else if (ataque == "Pegasus" && jogadaComp == "Unicórnio")
-            {
-                Console.WriteLine("O Pegasus é atingido pela luz desferida pelo unicórnio durante o voo. Unicórnio vence a rodada.");
-                hpjog = hpjog - 3;
-            }
-            else if (ataque == "Pegasus" && jogadaComp == "Grifo")
-            {
-                Console.WriteLine("O Grifo é cortado por uma arrancada do Pegasus vindo dos céus. Pegasus vence a rodada.");
-                hpcomp = hpcomp - 3;
-            }
-            else if (ataque == "Grifo" && jogadaComp == "Unicórnio")
-            {
-                Console.WriteLine("O Grifo repele o ataque com suas garras, refletindo-o na direção do Unicórnio. Grifo vence a rodada.");
-                hpcomp = hpcomp - 4;
-            }
-            else if (ataque == "Grifo" && jogadaComp == "Pegasus")
-            {
-                Console.WriteLine("O Grifo é cortado por uma arrancada do Pegasus vindo dos céus. Pegasus vence a rodada.");
-                hpjog = hpjog - 3;
-            }
-            else if (ataque == jogadaComp)
-            {
-                Console.WriteLine("Empate! Nenhum dos dois consegue vencer a rodada.");
-            }
-            else
-            {
-                Console.WriteLine("Jogada inválida. Tente novamente.");
-            }
-
-            string[,] hp =
-            {
-        {"HP do Jogador - ", hpjog.ToString() },
-        {"HP do Computador - ", hpcomp.ToString() },
-            };
-            Console.WriteLine("Status da batalha: ");
-            for (int i = 0; i < hp.GetLength(0); i++)
-            {
-                Console.WriteLine(hp[i, 0] + hp[i, 1]);
-
-            }
-            Console.WriteLine("==================================");
-
-
-        }
-        if (hpjog <= 0)
-        {
-            Console.WriteLine("O Computador venceu a batalha! Você foi derrotado, " + nome + ". Tente novamente na próxima vez.");
-        }
-        else if (hpcomp <= 0)
-        {
-            Console.WriteLine("Parabéns, " + nome + "! Você venceu a batalha e se tornou o maior domador do reino!");
-        }
-    }
-     
-
 }
